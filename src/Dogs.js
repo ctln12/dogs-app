dbd8359672deadb58d6c7ad1b8e8734b812d69db
Cataline Baert <snuf_kin@yahoo.com>
2021-02-02 21:23:37 +0100

    add more style to Dogs component

--- src/Dogs.js
+++ src/Dogs.js
@@ -7,14 +7,14 @@ class Dogs extends Component {
     const {dogs} = this.props;
     return (
       <div className="Dogs">
-        <h1 className="display-2 text-center">Choose a Dog!</h1>
+        <h1 className="display-3 text-center my-4">Choose a Dog!</h1>
         <div className="Dogs-list text-center">
           <div className="row">
             {dogs.map(dog => (
               <div className="Dogs-dog col-md-6 col-lg-4" key={dog.name}>
                 <img src={dog.src} alt={dog.name} />
                 <h3>
-                  <Link to={`/dogs/${dog.name}`}>
+                  <Link className="underline" to={`/dogs/${dog.name}`}>
                     {dog.name}
                   </Link>
                 </h3>
